aa8339b497fff9e351f5db4b847d5bbf2b647466
Derrick Kaffo <dkaffo@toltec-consulting.com>
2018-03-08 16:52:34 +0100

    RestContact

--- src/main/java/org/lepro/contact/dao/ContactRepository.java
+++ src/main/java/org/lepro/contact/dao/ContactRepository.java
@@ -1,8 +1,13 @@
 package org.lepro.contact.dao;
 
 import org.lepro.contact.entities.Contact;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ContactRepository extends JpaRepository<Contact, Long> {
-
+	@Query("select c from Contact c where c.nom like :x")
+	public Page<Contact> chercher(@Param("x") String mc, Pageable pageable);
 }
