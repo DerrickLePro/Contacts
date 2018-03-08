23807d00f2d3fdcd6cb973a635358886566eb76e
Derrick Kaffo <dkaffo@toltec-consulting.com>
2018-03-08 12:52:06 +0100

    initialize

---
+++ src/main/java/org/lepro/contact/SpContactApplication.java
@@ -0,0 +1,12 @@
+package org.lepro.contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpContactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpContactApplication.class, args);
+	}
+}
